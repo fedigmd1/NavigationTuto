d6a4e275b4a038ebb99033433c998fbf7c6e8af8
fedi.gmd <gammoudifedi@gmail.com>
2020-09-25 14:07:48 +0100

    add navigation dependencies

--- App.js
+++ App.js
@@ -1,11 +1,48 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+
+import Feed from './src/feed'
+import Details from './src/details'
+
+import Screen1 from './src/screens/drawer/screen1'
+import Screen2 from './src/screens/drawer/screen2'
+import Screen3 from './src/screens/drawer/screen3'
+
+import Tab1 from './src/screens/tabs/Tab1'
+import Tab2 from './src/screens/tabs/Tab2'
+import Tab3 from './src/screens/tabs/Tab3'
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createStackNavigator } from '@react-navigation/stack'
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+// import { View, Text } from 'react-native';
+
+const Drawer = createDrawerNavigator()
+const stack = createStackNavigator()
+const MaterialBottomTabs = createMaterialBottomTabNavigator()
+const MaterialTopTabs = createMaterialTopTabNavigator()
+
+
+
 const App = () => {
   return (
-    <View>
-      {/* <Feed /> */}
-      <Text></Text>
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Home"/>
+        <Drawer.Screen name="Contacts" component={Screen1} />
+        <Drawer.Screen name="Favorites" component={Screen2} />
+        <Drawer.Screen name="Settings" component={Screen3} />
+      </Drawer.Navigator>
+
+
+    </NavigationContainer>
+
+
+    // <View>
+    //   <Feed />
+    //   <Text>Hello World</Text>
+    // </View>
   );
 };
 export default App;
