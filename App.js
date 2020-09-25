cfe04b0845504e25c42ae7b397358b95be08ec5e
fedi.gmd <gammoudifedi@gmail.com>
2020-09-25 15:10:11 +0100

    add dependencies

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
-import Feed from './src/feed'
-import Details from './src/details'
+// import Feed from './src/feed'
+// import Details from './src/details'
 
-import Screen1 from './src/screens/drawer/screen1'
-import Screen2 from './src/screens/drawer/screen2'
-import Screen3 from './src/screens/drawer/screen3'
+import Screen1 from './src/screens/drawer/screen1';
+import Screen2 from './src/screens/drawer/screen2';
+import Screen3 from './src/screens/drawer/screen3';
 
-import Tab1 from './src/screens/tabs/Tab1'
-import Tab2 from './src/screens/tabs/Tab2'
-import Tab3 from './src/screens/tabs/Tab3'
+// import Tab1 from './src/screens/tabs/Tab1'
+// import Tab2 from './src/screens/tabs/Tab2'
+// import Tab3 from './src/screens/tabs/Tab3'
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { createStackNavigator } from '@react-navigation/stack'
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+import { createStackNavigator } from '@react-navigation/stack';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 // import { View, Text } from 'react-native';
 
 const Drawer = createDrawerNavigator()
